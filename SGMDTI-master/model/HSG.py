4732ea1365108ae373578d7e31ca71eae9c662f5
HaoYuanLi <1242884508@qq.com>
2024-10-12 08:38:11 +0800

    Update HSG.py

--- SGMDTI-master/model/HSG.py
+++ SGMDTI-master/model/HSG.py
@@ -1,6 +1,15 @@
 import os
 import numpy as np
 import pickle
+import os
+import random
+import numpy as np
+import networkx as nx
+import tensorflow as tf
+import warnings
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0' 
+tf.get_logger().setLevel('ERROR')
+warnings.filterwarnings('ignore')
 
 def _prep_hetSG_walks(walks, node2id, nodetypef):
     with open(nodetypef,'r') as fr:
@@ -80,3 +89,81 @@ def HSG(use_hetSG:bool, walks:list, nodes:set, nodetypef:str, embedding_size:int
                 embeddings[id2node[nodeid]]=embedding
 
     return embeddings
+                       
+def read_graph(edgeList,weighted=True, directed=False,delimiter='\t'):
+    '''
+    Reads the input network in networkx.
+    '''
+    if weighted:
+        G = nx.read_edgelist(edgeList, nodetype=str, 
+                             data=(('type',int),('weight',float),('id',int)), 
+                             create_using=nx.MultiDiGraph(),
+                            delimiter=delimiter)
+    else:
+        G = nx.read_edgelist(edgeList, nodetype=str,data=(('type',int)), 
+                             create_using=nx.MultiDiGraph(),
+                            delimiter=delimiter)
+        for edge in G.edges():
+            edge=G[edge[0]][edge[1]]
+            for i in range(len(edge)):
+                edge[i]['weight'] = 1.0
+    if not directed:
+        G = G.to_undirected()
+
+    return G
+
+def trans(data):
+    # fingerprint=df.values
+    A=AE(6,30,30,data)
+    return A
+
+def AE(layer, epochs, batch_size, mat_contents):
+    encoding_dim = 64
+    layer = int(layer)
+    epochs = int(epochs)
+    batch_size = int(batch_size)
+    L = [int(len(mat_contents[0]) // (2**i)) for i in range(layer)]
+
+    x_train_test = []
+    for row in mat_contents:  # 接口
+        a = row
+        x_train_test.append(a)
+
+    x_train_test = tf.convert_to_tensor(x_train_test)
+    x_train = x_train_test
+    # this is our input placeholder
+    input_img = tf.keras.layers.Input(shape=(L[0],))
+
+    # encoder layers
+    encoded = input_img
+    for i in range(layer-1):
+        encoded = tf.keras.layers.Dense(L[i+1], activation='relu')(encoded)
+
+    encoder_output = tf.keras.layers.Dense(encoding_dim)(encoded)
+
+    # decoder layers
+    decoded = encoder_output
+    for i in range(layer-1):
+        decoded = tf.keras.layers.Dense(L[layer-i-1], activation='relu')(decoded)
+
+    decoded = tf.keras.layers.Dense(L[0], activation='tanh')(decoded)
+    # construct the autoencoder model
+    autoencoder = tf.keras.models.Model(input_img, decoded)
+    encoder = tf.keras.models.Model(input_img, encoder_output)
+    # compile autoencoder
+    autoencoder.compile(optimizer=tf.keras.optimizers.Adam(), loss=tf.keras.losses.MeanSquaredError())
+    # training
+    autoencoder.fit(x_train, x_train,
+                    epochs=epochs,
+                    batch_size=batch_size,
+                    shuffle=True,
+                    verbose=0)
+    # plotting
+    encoded_imgs = encoder.predict(x_train)
+    # to xlsx
+    return encoded_imgs
+def set_seed(seed=42):
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    print(f'random seed with {seed}')
